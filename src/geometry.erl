70a33137f56887fe841d9ed457dc85e7c14b8a8b
Alopex6414 <1029535012@qq.com>
2019-05-12 15:01:47 +0800

    add geometry.erl

---
+++ src/geometry.erl
@@ -0,0 +1,37 @@
+%% @author 10295
+%% @doc @todo Add description to geometry.
+
+
+-module(geometry).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([area/1, area2/1]).
+
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% area function
+area({rectangle, Width, Height})	->
+	Width * Height;
+area({square, Side})				->
+	Side * Side;
+area({circle, Radius})				->
+	3.1415926 * Radius * Radius.
+
+% area2 function
+area2(T)	->
+	case T of
+		{rectangle, Width, Height}	->
+			Width * Height;
+		{square, Side}				->
+			Side * Side;
+		{circle, Radius}			->
+			3.1415926 * Radius * Radius;
+		_							->
+			undefined
+	end.
