def92d2c924448a47d3f0a73e45cda6be031e189
Alopex6414 <1029535012@qq.com>
2019-05-12 20:00:18 +0800

    add try_test.erl

---
+++ src/try_test.erl
@@ -0,0 +1,37 @@
+%% @author 10295
+%% @doc @todo Add description to try_test.
+
+
+-module(try_test).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([demo1/0]).
+
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% generate_exception function
+generate_exception(1)	->	a;
+generate_exception(2)	->	throw(a);
+generate_exception(3)	->	exit(a);
+generate_exception(4)	->	{'EXIT', a};
+generate_exception(5)	->	error(a).
+
+%% catcher function
+catcher(N)	->
+	try generate_exception(N) of
+		Val	-> {N, normal, Val}
+	catch
+		throw: 	X	-> {N, caught, thrown, X};
+		exit:	X	-> {N, caught, exited, X};
+		error:	X	-> {N, caught, error,  X}
+	end.
+
+%% demo1 function
+demo1()	->
+	[catcher(I)||I<-[1,2,3,4,5]].
