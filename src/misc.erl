f698c0e2f49532bbfecb8a75b10563c669017713
Alopex6414 <1029535012@qq.com>
2019-05-13 23:24:14 +0800

    modify misc.erl

--- src/misc.erl
+++ src/misc.erl
@@ -9,7 +9,8 @@
 %% ====================================================================
 -export([for/3]).
 -export([my_tuple_to_list/1]).
-
+-export([sleep/1]).
+-export([flush_buffer/0]).
 
 %% ====================================================================
 %% Internal functions
@@ -25,3 +26,19 @@ for(I, Max, F)		->
 my_tuple_to_list(T)	->
 	Count = tuple_size(T),
 	for(1, Count, fun(I) -> element(I, T) end).
+
+%% sleep function
+sleep(T)	->
+	receive
+		after T	->
+			true
+	end.
+
+%% flush_buffer function
+flush_buffer()	->
+	receive
+		_Any	->
+			flush_buffer()
+	after 0		->
+		true
+	end.
